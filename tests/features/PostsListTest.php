70b9020af76abf9fbf96b18b11fb186d874f26d8
Marcio Fuentes <marcio@adue.digital>
2019-08-26 14:21:02 -0300

    Lección 17

---
+++ tests/features/PostsListTest.php
@@ -0,0 +1,50 @@
+<?php
+
+use App\Post;
+use Carbon\Carbon;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class PostsListTest extends FeatureTestCase
+{
+    function test_a_user_can_see_the_posts_list_and_go_to_the_details()
+    {
+        $post = $this->createPost([
+            'title' => '¿Debo usar Laravel 5.3 o 5.1 LTS?'
+        ]);
+
+        $this->visit('/')
+            ->seeInElement('h1', 'Posts')
+            ->see($post->title)
+            ->click($post->title)
+            ->seePageIs($post->url);
+    }
+
+    function test_the_posts_are_paginated()
+    {
+        // Having
+        $first = factory(Post::class)->create([
+            'title' => 'Post más antiguo',
+            'created_at' => Carbon::now()->subDays(2)
+        ]);
+
+        factory(Post::class)->times(15)->create([
+            'created_at' => Carbon::now()->subDays(1)
+        ]);
+
+
+        $last = factory(Post::class)->create([
+            'title' => 'Post más reciente',
+            'created_at' => Carbon::now()
+        ]);
+
+        $this->visit('/')
+            ->see($last->title)
+            ->dontSee($first->title)
+            ->click(2)
+            ->see($first->title)
+            ->dontSee($last->title);
+
+    }
+}
