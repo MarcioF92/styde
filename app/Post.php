7d9dcca62d31b5631a65ac1c122be7ab8f32f9c6
Marcio Fuentes <marcio@adue.digital>
2019-09-27 08:43:11 -0300

    Lesson 23

--- app/Post.php
+++ app/Post.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use GrahamCampbell\Markdown\Facades\Markdown;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Str;
 
@@ -39,4 +40,10 @@ class Post extends Model
     {
         return route('posts.show', [$this->id, $this->slug]);
     }
+
+    public function getSafeHtmlContentAttribute()
+    {
+        return Markdown::convertToHtml(e($this->content));
+    }
+
 }
---
+++ tests/features/SupportMarkdownTest.php
@@ -0,0 +1,59 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class SupportMarkdownTest extends FeatureTestCase
+{
+    function test_the_post_content_support_markdown()
+    {
+        $importantText = 'Un texto muy importante';
+
+        $post = $this->createPost([
+            'content' => "La primera parte del texto. **$importantText**. La última parte del texto"
+        ]);
+
+        $this->visit($post->url)
+            ->seeInElement('strong', $importantText);
+    }
+
+    function test_the_code_in_the_post_is_escaped()
+    {
+        $xssAttack = "<script>alert('Malicious JS!')</script>";
+
+        $post = $this->createPost([
+            'content' => "$xssAttack. Texto normal."
+        ]);
+
+        $this->visit($post->url)
+            ->dontSee($xssAttack)
+            ->seeText('Texto normal.')
+            ->seeText($xssAttack);
+    }
+
+    function test_xss_attack()
+    {
+        $xssAttack = "<script>alert('Malicious JS!')</script>";
+
+        $post = $this->createPost([
+            'content' => "$xssAttack. Texto normal."
+        ]);
+
+        $this->visit($post->url)
+            ->dontSee($xssAttack)
+            ->seeText('Texto normal.');
+    }
+
+    function test_xss_attack_with_html()
+    {
+        $xssAttack = "<img src='img.jpg'>";
+
+        $post = $this->createPost([
+            'content' => "$xssAttack. Texto normal."
+        ]);
+
+        $this->visit($post->url)
+            ->dontSee($xssAttack);
+    }
+}
